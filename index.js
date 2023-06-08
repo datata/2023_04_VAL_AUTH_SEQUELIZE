a1830bd67ad982f74bc3f5db0ca8eae462e2704b
Dani <d.tarazoma@geekshubs.com>
2023-06-08 15:14:56 +0200

    changes

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const authController = require('./controllers/authController');
 
 const app = express();
 
-const PORT = process.env.PORT || 3000;;
+const PORT = 4000;;
 app.use(cors())
 app.use(express.json());
 
