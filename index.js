dfdb3aae83a1a9d9f523d65183fa49c6098acd2c
Dani <d.tarazoma@geekshubs.com>
2023-06-08 15:09:16 +0200

    feat: pruebas

--- index.js
+++ index.js
@@ -12,13 +12,12 @@ const PORT = 3000;
 app.use(cors())
 app.use(express.json());
 
-// gestiona las rutas de router.js
-app.use(router);
-
 app.get('/health', auth, (req, res) => {
     return res.send('healthy');
 });
 
+// gestiona las rutas de router.js
+app.use(router);
 
 db.then(() =>
     {
